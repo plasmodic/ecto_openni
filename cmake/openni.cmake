88934707c925096421b23d4e5ad404f595cadf74
Ethan Rublee <ethan.rublee@gmail.com>
2011-09-04 05:20:19 -0700

    quiet down now.

--- cmake/openni.cmake
+++ cmake/openni.cmake
@@ -1,6 +1,7 @@
 find_file(OPENNI_INCLUDES "XnCppWrapper.h" PATHS $ENV{OPEN_NI_INCLUDE} "/usr/include/ni" "/usr/include/openni" "c:/Program Files/OpenNI/Include" DOC "OpenNI c++ interface header")
 find_library(OPENNI_LIBRARY "OpenNI" PATHS $ENV{OPEN_NI_LIB} "/usr/lib" "c:/Program Files/OpenNI/Lib" DOC "OpenNI library")
-
+mark_as_advanced(OPENNI_INCLUDES)
+mark_as_advanced(OPENNI_LIBRARY)
 if(OPENNI_LIBRARY AND OPENNI_INCLUDES)
     set(HAVE_OPENNI TRUE)
     # the check: are PrimeSensor Modules for OpenNI installed
@@ -9,14 +10,14 @@ if(OPENNI_LIBRARY AND OPENNI_INCLUDES)
     elseif(UNIX OR APPLE)
         find_library(OPENNI_PRIME_SENSOR_MODULE "XnCore" PATHS "/usr/lib" DOC "Core library of PrimeSensor Modules for OpenNI")
     endif()
-	
+	mark_as_advanced(OPENNI_PRIME_SENSOR_MODULE)
     if(OPENNI_PRIME_SENSOR_MODULE)
         set(HAVE_OPENNI_PRIME_SENSOR_MODULE TRUE)
     endif()
 
-    get_filename_component(OPENNI_LIB_DIR "${OPENNI_LIBRARY}" PATH CACHE)
-    get_filename_component(OPENNI_INCLUDE_DIR ${OPENNI_INCLUDES} PATH CACHE)
-    get_filename_component(OPENNI_PRIME_SENSOR_MODULE_BIN_DIR "${OPENNI_PRIME_SENSOR_MODULE}" PATH CACHE)
+    get_filename_component(OPENNI_LIB_DIR "${OPENNI_LIBRARY}" PATH CACHE INTERNAL)
+    get_filename_component(OPENNI_INCLUDE_DIR ${OPENNI_INCLUDES} PATH CACHE INTERNAL)
+    get_filename_component(OPENNI_PRIME_SENSOR_MODULE_BIN_DIR "${OPENNI_PRIME_SENSOR_MODULE}" PATH CACHE INTERNAL)
 
 else() #if(OPENNI_LIBRARY AND OPENNI_INCLUDES)
   set(HAVE_OPENNI FALSE)
