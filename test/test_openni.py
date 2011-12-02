dbe2586f6b96ec7b1cdf8754a465d85865a0dce7
Ethan Rublee <ethan.rublee@gmail.com>
2011-12-01 17:21:30 -0800

    Tests.

--- test/test_openni.py
+++ test/test_openni.py
@@ -4,16 +4,24 @@ from ecto_opencv.highgui import imshow, NiConverter, FPSDrawer
 from ecto_opencv.imgproc import ConvertTo
 from ecto_opencv.cv_bp import CV_8UC1
 from ecto_openni import OpenNICapture, DEPTH_RGB
+import argparse
+from ecto.opts import cell_options
 
-capture = OpenNICapture(stream_mode=DEPTH_RGB, registration=True, sync=True)
+parser = argparse.ArgumentParser(description='My awesome program thing.')
+
+#add our cells to the parser
+camera_factory = cell_options(parser, OpenNICapture)
+args = parser.parse_args()
+print args
+
+#use the factories in conjunction with the parsed arguments, to create our cells.
+capture = camera_factory(args)
 fps = FPSDrawer('fps')
 plasm = ecto.Plasm()
 plasm.connect(capture['image'] >> fps[:],
               fps[:] >> imshow('image display', name='image')[:],
               capture['depth'] >> imshow('depth display', name='depth')[:],
               )
-
-#plasm.insert(capture)
 
 sched = ecto.schedulers.Singlethreaded(plasm)
 sched.execute()
