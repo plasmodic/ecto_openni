7723cbde3ef61f5b1ea1f7ec4c787bb572396a20
Vincent Rabaud <vincent.rabaud@gmail.com>
2015-05-08 18:30:29 +0200

    convert the test to proper nosetest

--- test/test_import.py
+++ test/test_import.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python
 
 import ecto_openni
-print ecto_openni
+import unittest
 
+class TestOpenni(unittest.TestCase):
+
+    def test_import(self):
+        print ecto_openni
+
+if __name__ == '__main__':
+    import rostest
+    rostest.rosrun('ecto_openni', 'test_openni', TestOpenni)
